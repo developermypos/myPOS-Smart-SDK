1fed78877480d487f45ebaeecab74b4f776cdeee
developermypos <yavor.petrov@icard.com>
2018-01-31 14:46:34 +0200

    add skip merchant / customer copy params for pre-auth cancellation

--- mypossmartsdk/src/main/java/com/mypos/smartsdk/MyPOSPreauthorizationCancellation.java
+++ mypossmartsdk/src/main/java/com/mypos/smartsdk/MyPOSPreauthorizationCancellation.java
@@ -8,12 +8,16 @@ public class MyPOSPreauthorizationCancellation {
     private boolean              motoTransaction;
     private String               foreignTransactionId;
     private String               preauthorizationCode;
+    private int                 printMerchantReceipt;
+    private int                 printCustomerReceipt;
 
 
     MyPOSPreauthorizationCancellation(MyPOSPreauthorizationCancellation.Builder builder) {
         this.foreignTransactionId = builder.foreignTransactionId;
         this.motoTransaction = builder.motoTransaction;
         this.preauthorizationCode = builder.preauthorizationCode;
+        this.printMerchantReceipt = builder.printMerchantReceipt;
+        this.printCustomerReceipt = builder.printCustomerReceipt;
     }
 
 
@@ -48,10 +52,31 @@ public class MyPOSPreauthorizationCancellation {
         return this;
     }
 
+    public int getPrintMerchantReceipt() {
+        return printMerchantReceipt;
+    }
+
+    public MyPOSPreauthorizationCancellation setPrintMerchantReceipt(int printMerchantReceipt) {
+        this.printMerchantReceipt = printMerchantReceipt;
+        return this;
+    }
+
+    public int getPrintCustomerReceipt() {
+        return printCustomerReceipt;
+    }
+
+    public MyPOSPreauthorizationCancellation setPrintCustomerReceipt(int printCustomerReceipt) {
+        this.printCustomerReceipt = printCustomerReceipt;
+        return this;
+    }
+
+
     public static class Builder {
         private boolean              motoTransaction;
         private String               foreignTransactionId;
         private String               preauthorizationCode;
+        private int                 printMerchantReceipt;
+        private int                 printCustomerReceipt;
 
         public MyPOSPreauthorizationCancellation.Builder foreignTransactionId(String foreignTransactionId) {
             this.foreignTransactionId = foreignTransactionId;
@@ -68,7 +93,15 @@ public class MyPOSPreauthorizationCancellation {
             return this;
         }
 
+        public MyPOSPreauthorizationCancellation.Builder printMerchantReceipt(int printMerchantReceipt) {
+            this.printMerchantReceipt = printMerchantReceipt;
+            return this;
+        }
 
+        public MyPOSPreauthorizationCancellation.Builder printCustomerReceipt(int printCustomerReceipt) {
+            this.printCustomerReceipt = printCustomerReceipt;
+            return this;
+        }
         public MyPOSPreauthorizationCancellation build() {
             if (this.preauthorizationCode == null || preauthorizationCode.isEmpty()) {
                 throw new IllegalArgumentException("Missing preauthorization code");
