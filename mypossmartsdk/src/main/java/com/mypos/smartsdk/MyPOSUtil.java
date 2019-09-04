7ddf4d0f35ffa54173edfbf5b6c4543f6159f47d
developermypos <yavor.petrov@icard.com>
2019-09-04 11:54:02 +0300

    reference number max len = 50

--- mypossmartsdk/src/main/java/com/mypos/smartsdk/MyPOSUtil.java
+++ mypossmartsdk/src/main/java/com/mypos/smartsdk/MyPOSUtil.java
@@ -149,7 +149,7 @@ public class MyPOSUtil {
     public static final String INTENT_SAM_CARD_COMMAND_ISOCOMMAND   = "isoCommand";
 
     public static boolean isReferenceNumberValid(String referenceNumber) {
-        return referenceNumber == null || (referenceNumber.length() <= 20 && referenceNumber.matches("[a-zA-Z0-9\\p{Punct}]+"));
+        return referenceNumber == null || (referenceNumber.length() <= 50 && referenceNumber.matches("[a-zA-Z0-9\\p{Punct}]+"));
     }
 
 }
